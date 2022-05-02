cfa37c219e7cb7df2efe3886929c4abca9bf43bb
Laimonas Remeikis <laimonasremeikis@icloud.com>
2022-05-02 10:13:14 +0300

    movie title fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import {useMovies} from "./hooks";
 import {Autocomplete} from "./components";
 import {useState} from "react";
+import star from './img/star.svg';
+
+
 
 const App = () => {
   const [query, setQuery] = useState('');
   const {movies, movie, getMovie} = useMovies(query);
+  
 
   const handleOnSelect = async (movie) => {
     await getMovie(movie.id);
+    setQuery(movie.title)
   };
 
+  
+
   return (
-    <div className="app">
+    <div className="app" >
       <header className="header">
         <div className="container">
           <Autocomplete
@@ -20,15 +27,30 @@ const App = () => {
             onQueryChange={setQuery}
             options={movies}
           />
+          
         </div>
       </header>
       {!!movie && (
-        <main className="main">
-          <div className="container">
+        <div className="card">
+        <main className="main" >
+          <div className="Movie">
             <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
-            {JSON.stringify(movie)}
-          </div>
+            </div>
+            <div className="info">
+        <h3 className="title">{movie.title} ({movie.release_date ? movie.release_date.substring(0, 4) : '-'})</h3>
+        <p className="language">Original language: {movie.original_language.toUpperCase()}</p>
+        <div className="vote">
+          <img className='starImg' style={{width: '24px', heigth: '24px'}} src={star} alt='star'></img>
+            <div>
+              <p style={{fontWeight: '700'}}>{movie.vote_average}/10</p>
+              <p style={{fontSize: '10px', color: '#a4a4a4'}}>{movie.vote_count} votes</p>
+            </div>
+        </div>
+        <p className='overview'>{movie.overview ? movie.overview : 'No description'}</p>
+      </div>
+        
         </main>
+        </div>
       )}
     </div>
   );
--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -4,29 +4,45 @@ const Autocomplete = (props) => {
   const {query, onQueryChange, options, onSelect} = props;
   const [optionsVisible, setOptionsVisible] = useState(false);
 
+  const close = () => {
+    setOptionsVisible(false)
+  }
+  const handleInputChange = (event) => {
+    onQueryChange(event.currentTarget.value)
+  event.currentTarget.value.length >= 3 &&  setOptionsVisible(true)
+  }
+
+ 
+
   return (
-    <div className="autocomplete">
-      <input
-        onFocus={() => setOptionsVisible(true)}
-        onBlur={() => setOptionsVisible(false)}
+    <div className="autocomplete" onClick={close}>
+      <input 
+      type= 'search'
+        // onFocus={() => setOptionsVisible(true)}
+        // onBlur={() => setOptionsVisible(false)}
         className="autocomplete-search"
         value={query}
-        onChange={(event) => onQueryChange(event.currentTarget.value)}
+        onChange={(event) => handleInputChange(event)}
       />
-      <div className="autocomplete-options">
+      {optionsVisible && <div className="autocomplete-options">
         {options.length === 0 && query.length >= 3 && <div className="autocomplete-empty">No options</div>}
         {options.map((option, index) => (
           <div
             key={index}
             onClick={() => onSelect(option)}
             className="autocomplete-option">
-            <span>{option.title}</span>
+            <h4>{option.title}</h4>
+            <p style={{color: '#3d3c3c', fontSize: '13px'}}>{option.vote_average} Rating, {option.release_date ? option.release_date.substring(0, 4) : '-'}</p>
           </div>
         ))}
-      </div>
+      </div>}
     </div>
   )
 
 };
 
 export default Autocomplete;
+
+
+
+
